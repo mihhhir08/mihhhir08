ecf812c206d0ff11b659cd0d63b2fd4be99fa245
Mihirsinh Chavda <mihhhir08@gmail.com>
2024-09-21 16:29:27 -0400

    Add files via upload

---
+++  Portfolio/script.js
@@ -0,0 +1,21 @@
+// Add smooth scrolling for the navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+/* JavaScript for reveal on scroll */
+document.addEventListener('scroll', () => {
+    document.querySelectorAll('.section').forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        if (sectionTop < window.innerHeight * 0.75) {
+            section.classList.add('visible');
+        }
+    });
+});
